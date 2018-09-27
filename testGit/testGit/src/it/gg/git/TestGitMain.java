1c4ed11963ccff913a42a2defc986ee161e84e2f
Gianluca <Gianluca@172.19.243.210>
2018-09-27 16:22:11 +0200

    test GIT

---
+++ testGit/testGit/src/it/gg/git/TestGitMain.java
@@ -0,0 +1,14 @@
+package it.gg.git;
+
+public class TestGitMain {
+
+	public static void main(String[] args) {
+		
+		Persona pers = new Persona();
+		pers.setName("Gianluca");
+		pers.setSurname("Germani");
+		
+
+	}
+
+}
